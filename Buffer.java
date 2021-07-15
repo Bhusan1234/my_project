1d2ec6812f96164ff945387178af90285c1e3f80
Biswabhusan Dixit <biswabhusandixit@gmail.com>
2021-07-15 22:03:42 +0530

    new commit

---
+++ Buffer.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Bufer{
+public static void main(String args[]){
+  FileWriter writer=new FileWriter("C:\Users\Dell\Desktop\my details\text.txt");
+   BufferedWriter buffer=new BufferedWriter(writer);
+   buffer.write("welcome to jaav");
+   buffer.close();
+   System.out.println("ducess");   
+}
+
+}
